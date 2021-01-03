07f60f0ebe9fc0e6c36f265635c1a5b8b7ab0e45
sk97kang <sk97.kang@gmail.com>
2021-01-03 14:06:51 +0900

    #9 USER MODULE E2E

---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,305 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { getConnection, Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Verification } from 'src/users/entities/verification.entity';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
+const testUser = {
+  email: 'test@test.com',
+  password: '12345',
+};
+
+describe('UsersModule (e2e)', () => {
+  let app: INestApplication;
+  let usersRepository: Repository<User>;
+  let verificationRepository: Repository<Verification>;
+  let jwtToken: string;
+  const graphqlRequest = (query: string) =>
+    request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({ query });
+  const graphqlAuthRequest = (query: string) =>
+    request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .set('X-JWT', jwtToken)
+      .send({ query });
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    usersRepository = module.get<Repository<User>>(getRepositoryToken(User));
+    verificationRepository = module.get<Repository<Verification>>(
+      getRepositoryToken(Verification),
+    );
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    await app.close();
+  });
+
+  describe('createAccount', () => {
+    it('should create account', () => {
+      return graphqlRequest(`mutation {
+          createAccount(input: {
+            email:"${testUser.email}"
+            password:"${testUser.password}"
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }`)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { createAccount },
+            },
+          } = res;
+          expect(createAccount.ok).toBe(true);
+          expect(createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return graphqlRequest(`mutation {
+        createAccount(input: {
+          email:"${testUser.email}"
+          password:"${testUser.password}"
+          role:Owner
+        }) {
+          ok
+          error
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { createAccount },
+            },
+          } = res;
+          expect(createAccount.ok).toBe(false);
+          expect(createAccount.error).toBe(
+            'There is a user with that email already',
+          );
+        });
+    });
+  });
+
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return graphqlRequest(`mutation {
+          login(input: {
+            email:"${testUser.email}"
+            password:"${testUser.password}"
+          }) {
+            ok
+            error
+            token
+          }
+        }`)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+
+    it('should not be albe to login with wrong credentials', () => {
+      return graphqlRequest(`mutation {
+        login(input: {
+          email:"${testUser.email}"
+          password:"123123"
+        }) {
+          ok
+          error
+          token
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('Wrong password');
+          expect(login.token).toBe(null);
+        });
+    });
+  });
+
+  describe('userProfile', () => {
+    let userId: number;
+    beforeAll(async () => {
+      const [user] = await usersRepository.find();
+      userId = user.id;
+    });
+
+    it("should see a user's profile", () => {
+      return graphqlAuthRequest(`{
+        userProfile(userId:${userId}) {
+          ok
+          error
+          user {
+            id
+          }
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const { ok, error, user } = res.body.data.userProfile;
+          const { id } = user;
+          expect(ok).toBe(true);
+          expect(error).toBe(null);
+          expect(id).toBe(userId);
+        });
+    });
+
+    it('should not find a profile', () => {
+      return graphqlAuthRequest(`{
+        userProfile(userId:999) {
+          ok
+          error
+          user {
+            id
+          }
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const { ok, error, user } = res.body.data.userProfile;
+          expect(ok).toBe(false);
+          expect(error).toBe('User Not Found');
+          expect(user).toBe(null);
+        });
+    });
+  });
+
+  describe('me', () => {
+    it('should find my profile', () => {
+      return graphqlAuthRequest(`{
+        me {
+          email
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const { email } = res.body.data.me;
+          expect(email).toBe(testUser.email);
+        });
+    });
+
+    it('should not allow logged out user', () => {
+      return graphqlRequest(`{
+        me {
+          email
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const { errors } = res.body;
+          const [error] = errors;
+          expect(error.message).toBe('Forbidden resource');
+        });
+    });
+  });
+
+  describe('editProfile', () => {
+    const NEW_EMAIL = 'test@new.com';
+    it('should change email', () => {
+      return graphqlAuthRequest(`mutation {
+        editProfile(input: {
+          email: "${NEW_EMAIL}"
+        }) {
+          ok
+          error
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const { ok, error } = res.body.data.editProfile;
+          expect(ok).toBe(true);
+          expect(error).toBe(null);
+        });
+    });
+
+    it('should have new email', () => {
+      return graphqlAuthRequest(`{
+        me {
+          email
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const { email } = res.body.data.me;
+          expect(email).toBe(NEW_EMAIL);
+        });
+    });
+  });
+
+  describe('verifyEmail', () => {
+    let verificationCode: string;
+    beforeAll(async () => {
+      const [verification] = await verificationRepository.find();
+      verificationCode = verification.code;
+    });
+
+    it('should verify email', () => {
+      return graphqlRequest(`mutation {
+        verifyEmail(input: {
+          code: "${verificationCode}"
+        }) {
+          ok
+          error
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const { ok, error } = res.body.data.verifyEmail;
+          expect(ok).toBe(true);
+          expect(error).toBe(null);
+        });
+    });
+
+    it('should fail on wrong verification code not found', () => {
+      return graphqlRequest(`mutation {
+        verifyEmail(input: {
+          code: "123"
+        }) {
+          ok
+          error
+        }
+      }`)
+        .expect(200)
+        .expect((res) => {
+          const { ok, error } = res.body.data.verifyEmail;
+          expect(ok).toBe(false);
+          expect(error).toBe('Verification not found.');
+        });
+    });
+  });
+});
